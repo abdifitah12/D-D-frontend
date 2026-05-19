77f926c5d238838de0f9c8359627dea6e054bd79
abdihaashin <abdihaashin275@gmail.com>
2026-05-19 11:50:15 -0700

    past record

--- src/pages/LivePage.jsx
+++ src/pages/LivePage.jsx
@@ -6,13 +6,15 @@ export default function LivePage() {
         <h1 className="mb-6 text-center text-5xl font-bold text-white">
           🔴 D&D Café Live
         </h1>
-         <iframe
-                className="w-full h-[700px] rounded-3xl"
-                src="https://www.youtube.com/embed/live_stream?channel=UCTcF9kWeG4Obd9LxaKPa1mA"
-                title="D&D Café Live"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                />
+
+        <iframe
+          className="w-full h-[700px] rounded-3xl"
+          src="https://www.youtube.com/embed?listType=user_uploads&list=abdihaashin1350"
+          title="D&D Café Live"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+
       </div>
     </section>
   );
